1ae9efbc35005cc9e4f3e9720897634c51145227
genomics-kl <kin.lau@vai.org>
2025-04-09 00:43:26 -0400

    Fix make_mqc_sample_filters.R bugs

--- bin/scripts/make_mqc_sample_filters.R
+++ bin/scripts/make_mqc_sample_filters.R
@@ -16,18 +16,18 @@ library(readr)
 
 meta <- read_tsv(samplesheet) %>%
     dplyr::mutate(mqc_group = case_when(
-        sample %in% str_split_1(noncontrol_samples, ",") ~ "Controls",
+        !sample %in% str_split_1(noncontrol_samples, ",") ~ "Controls",
         .default = sample_group
-))
+)) %>%
     dplyr::select(-fq1, -fq2) %>%
     unique()
 
-fileConn <- file(out_sample_filts)
+fileConn <- file(out_sample_filts, "a") # append mode
 mqc_groups <- unique(meta$mqc_group)
 for (i in 1:length(mqc_groups)){
     curr_group <- mqc_groups[i]
-    curr_samples <- meta$sample[which(meta$mqc_group == curr_group), ]
-    outline <- paste(c(curr_group, "show", curr_samples), sep="\\t")
+    curr_samples <- meta$sample[which(meta$mqc_group == curr_group)]
+    outline <- paste(c(curr_group, "show", curr_samples), collapse="\t")
     writeLines(outline, fileConn)
 }
 
